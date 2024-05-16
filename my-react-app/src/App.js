b20df1f170c596a729bfbe8a6831a071b1f6f0e6
huiyingstella <148087511+huiyingstella@users.noreply.github.com>
2024-05-16 15:58:12 +0100

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import background from './img/background.png'
+import Navigation from './Components/Navigation';
+import Smallnav from './Components/Smallnav';
+import BodyContent from './Components/BodyContent';
+import FooterContent from './Components/FooterContent';
+
+
+function App() {
+  return (
+<AppStyled>
+  <Navigation/>
+  <Smallnav/>
+  <BodyContent/>
+  <FooterContent/>
+</AppStyled>
+  );
+}
+const AppStyled = styled.nav`
+width: 100%;
+height: 35rem;
+background-image: url(${background}); 
+background-repeat: no-repeat;
+background-size: cover;
+background-position: center; 
+background-attachment: fixed; 
+`
+export default App;
